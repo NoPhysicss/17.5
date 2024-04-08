10fb0a3248a41852db213e4ffc4f233aa060b374
alex <a>
2024-04-08 10:34:16 +0400

    Добавьте файлы проекта.

---
+++ 17.5/17.5.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+class Vector
+{
+private:
+    double x;
+    double y;
+    double z;
+public:
+
+    Vector() : x(0), y(0), z(0)
+    {}
+    Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z)
+    {}
+    void show()
+    {
+        std::cout << std::endl << x << " " << y << " " << z;
+    }
+    void mul()
+    {
+        std::cout << std::endl << (x + y + z);
+    }
+};
+
+int main()
+{
+    std::cout << "set Vectors\n";
+    double x = 0;
+    double y = 0;
+    double z = 0;
+
+    std::cout << "x = ";
+    std::cin >> x;
+    std::cout  << "y = ";
+    std::cin >> y;
+    std::cout  << "z = ";
+    std::cin >> z;
+
+    Vector V1(x, y, z);
+
+    V1.show();
+
+    V1.mul();
+
+
+
+
+}
+
